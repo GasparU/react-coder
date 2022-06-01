7fa8d4465e82c01f62a104b225e00094367f0f0f
Jose Gaspar <sainc2016@gmail.com>
2022-05-31 21:43:05 -0500

    modificando React.Fragment a <></>

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -3,6 +3,6 @@ import React from 'react'
 export default function ItemListContainer({ greeting }) {
 
   return (
-    <React.Fragment>Esto es un {greeting}</React.Fragment>
+    <>Esto es un {greeting}</>
   )
 }
